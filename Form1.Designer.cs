51ca74ab643e526270afdef0e92e4ccff59b72aa
assp <assp08@bk.ru>
2026-03-12 02:03:06 +0300

    Создан графический интерфейс

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.fromCurrencyComboBox = new System.Windows.Forms.ComboBox();
+            this.toCurrencyComboBox = new System.Windows.Forms.ComboBox();
+            this.amountTextBox = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // fromCurrencyComboBox
+            // 
+            this.fromCurrencyComboBox.FormattingEnabled = true;
+            this.fromCurrencyComboBox.Items.AddRange(new object[] {
+            "USD",
+            "EUR"});
+            this.fromCurrencyComboBox.Location = new System.Drawing.Point(12, 12);
+            this.fromCurrencyComboBox.Name = "fromCurrencyComboBox";
+            this.fromCurrencyComboBox.Size = new System.Drawing.Size(121, 21);
+            this.fromCurrencyComboBox.TabIndex = 0;
+            // 
+            // toCurrencyComboBox
+            // 
+            this.toCurrencyComboBox.FormattingEnabled = true;
+            this.toCurrencyComboBox.Items.AddRange(new object[] {
+            "USD",
+            "EUR"});
+            this.toCurrencyComboBox.Location = new System.Drawing.Point(152, 12);
+            this.toCurrencyComboBox.Name = "toCurrencyComboBox";
+            this.toCurrencyComboBox.Size = new System.Drawing.Size(121, 21);
+            this.toCurrencyComboBox.TabIndex = 1;
+            // 
+            // amountTextBox
+            // 
+            this.amountTextBox.Location = new System.Drawing.Point(12, 39);
+            this.amountTextBox.Name = "amountTextBox";
+            this.amountTextBox.Size = new System.Drawing.Size(261, 20);
+            this.amountTextBox.TabIndex = 2;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(12, 65);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(261, 23);
+            this.convertButton.TabIndex = 3;
+            this.convertButton.Text = "Конвертировать";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(12, 91);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(60, 13);
+            this.resultLabel.TabIndex = 4;
+            this.resultLabel.Text = "Результат:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 121);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.amountTextBox);
+            this.Controls.Add(this.toCurrencyComboBox);
+            this.Controls.Add(this.fromCurrencyComboBox);
+            this.Name = "Form1";
+            this.Text = "Конвертер валют";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox fromCurrencyComboBox;
+        private System.Windows.Forms.ComboBox toCurrencyComboBox;
+        private System.Windows.Forms.TextBox amountTextBox;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Label resultLabel;
     }
 }
-
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using CurrencyConverterApp;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        private CurrencyConverter converter;
+
         public Form1()
         {
+            converter = new CurrencyConverter();
             InitializeComponent();
+        }
+
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            // Проверка: введена ли сумма
+            if (string.IsNullOrEmpty(amountTextBox.Text))
+            {
+                MessageBox.Show("Введите сумму для конвертации!", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Проверка: корректный ли формат числа
+            if (!decimal.TryParse(amountTextBox.Text, out decimal amount))
+            {
+                MessageBox.Show("Неверный формат суммы! Введите число.", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Получение выбранных валют
+            string fromCurrency = fromCurrencyComboBox.SelectedItem?.ToString();
+            string toCurrency = toCurrencyComboBox.SelectedItem?.ToString();
+
+            // Проверка: выбраны ли валюты
+            if (string.IsNullOrEmpty(fromCurrency) || string.IsNullOrEmpty(toCurrency))
+            {
+                MessageBox.Show("Выберите валюты для конвертации!", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Попытка конвертации
+            try
+            {
+                decimal result = converter.Convert(amount, fromCurrency, toCurrency);
+                resultLabel.Text = $"Результат: {amount} {fromCurrency} = {result:F2} {toCurrency}";
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка ввода",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (NotSupportedException ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Непредвиденная ошибка: {ex.Message}", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
